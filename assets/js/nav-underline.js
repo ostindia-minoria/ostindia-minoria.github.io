e3a7b8b13843a5366ca858fe2bc981721c255f4e
ostindia-minoria <bsdinat@gmail.com>
2025-06-20 20:56:04 +0300

    Delete assets directory

--- assets/js/nav-underline.js
+++
@@ -1,7 +0,0 @@
-
-document.querySelectorAll(".nav-link").forEach(link => {
-  link.addEventListener("click", () => {
-    document.querySelectorAll(".nav-link").forEach(l => l.classList.remove("active"));
-    link.classList.add("active");
-  });
-});
